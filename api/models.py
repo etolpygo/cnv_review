2fd1c27de4ada8943078e811d056516b39fa49f4
Elena Tolpygo Cranley <elena.cranley@ucsf.edu>
2017-09-11 16:23:37 -0700

    add Case model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Case(models.Model):
+    TestOrderID = models.CharField(max_length=20, primary_key=True)
+    PatientID = models.CharField(max_length=20, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ('created',)
