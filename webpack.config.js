119eec63558ac74b1720703d00c56f136ea1dcd3
Elena Tolpygo Cranley <elena.cranley@ucsf.edu>
2017-09-11 17:10:28 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// require dependencies
+var path = require('path')
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+    // the base directory (absolute path) for resolving the entry option
+    context: __dirname,
+    // the entry point we created earlier
+    // ./ means your current directory
+    entry: './assets/js/index.js',
+    
+    output: {
+        // where you want the compiled bundle to be stored
+        path: path.resolve('./assets/bundles/'),
+        // naming convention webpack should use for your files
+        filename: '[name]-[hash].js',
+    },
+
+    plugins: [
+        // tells webpack where to store data about your bundles
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new webpack.ProvidePlugin({ 
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery' 
+        })
+    ],
+
+    module: {
+        loaders: [
+            // a regexp that tells webpack to use the following loaders on all .js and .jsx files
+            { test: /\.jsx?$/,
+                // we don't want babel to transpile all the files in node_modules, would take a long time
+                exclude: /node_modules/,
+                // use the babel loader
+                loader: 'babel-loader',
+                query: {
+                    // specify that we will be dealing with React code
+                    presets: ['react']
+                }
+            }
+        ]
+    }
+}
