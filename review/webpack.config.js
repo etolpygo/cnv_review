c2a07ce585feaa314a0ad105fe4b5c09d1d70277
Elena Tolpygo Cranley <elena.cranley@ucsf.edu>
2017-09-28 10:50:13 -0700

    add css loader and file loader into webpack so that we can use bootstrap

--- review/webpack.config.js
+++ review/webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
+
     // the base directory (absolute path) for resolving the entry option
     context: __dirname,
     // the entry point we created earlier
@@ -23,11 +24,6 @@ module.exports = {
     plugins: [
         // tells webpack where to store data about your bundles
         new BundleTracker({filename: './review/webpack-stats.json'}),
-        new webpack.ProvidePlugin({ 
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery' 
-        })
     ],
 
     module: {
@@ -35,13 +31,23 @@ module.exports = {
             // a regexp that tells webpack to use the following loaders on all .js and .jsx files
             { test: /\.jsx?$/,
                 // we don't want babel to transpile all the files in node_modules, would take a long time
-                exclude: /node_modules/,
+                exclude: ['/node_modules/', /\.css$/, /(\/fonts)/],
                 // use the babel loader
                 loader: 'babel-loader',
                 query: {
                     // specify that we will be dealing with React code
-                    presets: ['react']
+                    presets: ['es2015', 'react']
                 }
+            },
+            {
+               test: /\.css$/,
+               exclude: ['/node_modules/', /\.jsx?$/, /(\/fonts)/],
+               loader: 'css-loader'
+            },
+            {   
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, 
+                exclude: [/\.css$/, /\.jsx?$/],
+                loader: 'file-loader'
             }
         ]
     }
