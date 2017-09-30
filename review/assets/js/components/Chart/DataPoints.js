79734f86e2468918c264e609446d2b0b0ffce4ec
Elena Tolpygo Cranley <elena.cranley@ucsf.edu>
2017-09-29 17:43:21 -0700

    (tentatively) incorporate data points' weight value to modulate line opacity

--- review/assets/js/components/Chart/DataPoints.js
+++ review/assets/js/components/Chart/DataPoints.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const renderLines = (props) => {
 	return (coords, index) => {
+		const rgbaVal = "rgba(0, 0, 0, " + coords.weight + ")"
 		const lineProps = {
 			x1: props.xScale(coords.absoluteStart),
 			y1: props.yScale(coords.log2),
 			x2: props.xScale(coords.absoluteEnd),
 			y2: props.yScale(coords.log2),
-			strokeWidth: 1,
-			stroke: 'black',
-			fill: 'black',
+			strokeWidth: 2,
+			stroke: rgbaVal,
+			fill: rgbaVal,
 			key: index,
 			clipPath: props.clipPath
 		};
