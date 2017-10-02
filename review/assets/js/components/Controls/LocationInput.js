d5427f32f90124ccacc14396e436517d5e91037f
Elena Tolpygo Cranley <elena.cranley@ucsf.edu>
2017-10-02 11:01:27 -0700

    have the LocationInput component maintain its own state and update it based on user input

--- review/assets/js/components/Controls/LocationInput.js
+++ review/assets/js/components/Controls/LocationInput.js
@@ -4,7 +4,14 @@ export default class LocationInput extends React.Component {
 
 	constructor(props) {
 	    super(props);
+	    this.state = { vizLocation: '' };
+
+	    this.handleChange = this.handleChange.bind(this);
 	    this.handleKeyPress = this.handleKeyPress.bind(this);
+	}
+
+	handleChange(e) {
+	    this.setState({ vizLocation: e.target.value });
 	}
 
 	handleKeyPress(e) {
@@ -15,7 +22,13 @@ export default class LocationInput extends React.Component {
 
 	render() {
 		return(
-			<input type="text" className="col-xs-10" placeholder="chr1" onKeyPress={this.handleKeyPress} />
+			<input type="text" 
+				   value={this.state.vizLocation} 
+				   onChange={this.handleChange} 
+				   onKeyPress={this.handleKeyPress}
+				   className="col-xs-10" 
+				   placeholder="chr1" 
+				/>
 		);
 	}
 
